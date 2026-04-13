efe0925d338fb1833ce855562c1d14924a5aca86
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-04-13 12:54:52 +0300

    int/analyzer: inits

---
+++ pkg/analyzer/analyzer.go
@@ -0,0 +1,14 @@
+package analyzer
+
+import (
+	"github.com/ufukty/golits/internal/inspect"
+	"golang.org/x/tools/go/analysis"
+)
+
+func New() *analysis.Analyzer {
+	return &analysis.Analyzer{
+		Name: "golits",
+		Doc:  "Lists the string literals declared multiple times in a file.",
+		Run:  inspect.Files,
+	}
+}
