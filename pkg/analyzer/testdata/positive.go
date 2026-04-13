53cdc4a4baf4a0da9016511c50671bfebd36d98c
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-04-13 13:23:29 +0300

    pkg/analyzer/testdata: adds assertions to test file.

--- pkg/analyzer/testdata/positive.go
+++ pkg/analyzer/testdata/positive.go
@@ -3,15 +3,15 @@ package testdata
 import "fmt"
 
 var (
-	ErrA = fmt.Errorf("a")
-	ErrB = fmt.Errorf("a")
+	ErrA = fmt.Errorf("a") // want `duplicated string literal`
+	ErrB = fmt.Errorf("a") // want `duplicated string literal`
 	ErrC = fmt.Errorf("c")
-	ErrD = fmt.Errorf("d")
+	ErrD = fmt.Errorf("d") // want `duplicated string literal`
 )
 
 var (
 	ErrE = fmt.Errorf("e")
-	ErrF = fmt.Errorf("a")
+	ErrF = fmt.Errorf("a") // want `duplicated string literal`
 	ErrG = fmt.Errorf("g")
-	ErrH = fmt.Errorf("d")
+	ErrH = fmt.Errorf("d") // want `duplicated string literal`
 )
