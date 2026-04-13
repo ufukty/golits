78453ea1d5d837cabbb0d9cd00191e77280b52c6
Ufuktan Yıldırım <13807261+ufukty@users.noreply.github.com>
2026-04-13 13:19:54 +0300

    pkg/analyzer: changes the test to work with `analysistest`.

--- pkg/analyzer/inspect_test.go
+++ pkg/analyzer/inspect_test.go
@@ -1,22 +1,11 @@
-package inspect
+package analyzer
 
 import (
-	"os"
+	"testing"
+
+	"golang.org/x/tools/go/analysis/analysistest"
 )
 
-func ExampleFile_positive() {
-	fh, _ := os.Open("testdata/positive.go")
-	defer fh.Close()
-
-	duplicates, _ := File(fh, "positive.go")
-	basicPrinter(duplicates)
-
-	// Output:
-	// "a"
-	//   positive.go:6:20
-	//   positive.go:7:20
-	//   positive.go:14:20
-	// "d"
-	//   positive.go:9:20
-	//   positive.go:16:20
+func TestInspect(t *testing.T) {
+	analysistest.Run(t, analysistest.TestData(), New(), "")
 }
